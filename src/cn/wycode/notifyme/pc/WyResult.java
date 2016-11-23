466603ebf093a1125caff6c34cd21ca279f04b2e
王郁 <www_wayne_001@163.com>
2016-11-23 17:51:58 +0800

    Create WyResult.java

---
+++ src/cn/wycode/notifyme/pc/WyResult.java
@@ -0,0 +1,11 @@
+package cn.wycode.notifyme.pc.bean;
+
+/**
+ *
+ * Created by wWX383516 on 2016/11/23.
+ */
+public class WyResult<T> {
+    public int code;
+    public String message;
+    public T data;
+}
